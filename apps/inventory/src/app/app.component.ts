42a6f0c9e298ef1955ea33c815196e748fea6223
bartosz.janiak <bartosz.janiak@aug-e.io>
2024-10-07 14:20:24 +0200

    first commit

---
+++ apps/inventory/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductsComponent } from '@angular-monorepo/products';
+
+@Component({
+  standalone: true,
+  imports: [ProductsComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'inventory';
+}
